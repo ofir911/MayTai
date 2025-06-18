ecdfbd66695e3e8c0d53d186d8454ef77ed62a15
ofir911 <ofirmay@gmail.com>
2025-06-19 02:08:16 +0300

    Add files via upload

---
+++ Runtime.ps1
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 HelveticaNeue;}
+{\colortbl;\red255\green255\blue255;\red27\green27\blue27;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c14118\c14118\c14118;\cssrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+<runtime>\cb1 \
+\
+\cb3 \'a0 \'a0 <generatePublisherEvidence enabled="false"/>\cb1 \
+\
+\cb3 </runtime>}
